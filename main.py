a5f5a5a7e12e40fd99ce9ce939327fde7eef5db1
Spooks_01 <samfish1998@gmail.com>
2023-07-29 13:14:07 +0100

    remove bugged image loading

--- main.py
+++ main.py
@@ -71,19 +71,7 @@ def prepare_loaderxbe( self ):
 		attach_xbe_file.write( MOV_Fix )
 
 	attach_xbe_file.close()
-	##
-	try: # this is to move on if there is an error with extracting the image.
-		xbeinfo( os.path.join( Game_Path,"default.xbe" ) ).image_png()
-	except:
-		print "| Error: Memory ran out when trying to extract TitleImage.xbx."
-		print "|        So using alternative way."
-		try: # if the memory runs out this one works.
-			XBE( os.path.join( Game_Path,"default.xbe" ) ).Get_title_image().Write_PNG( "Z:\\default.png" )
-		except:
-			print "| Error: Cannot extract the default.png, haven't a clue why maybe its in DDS format?"
-	if os.path.isfile( 'Z:\\default.png' ):	shutil.move( 'Z:\\default.png',os.path.join( Game_Path,'default.tbn' ) )
-	if os.path.isfile( os.path.join( Game_Path,'default.tbn' ) ):	shutil.copy2( os.path.join( Game_Path,'default.tbn' ),os.path.join( Game_Path,'icon.png' ) )
-	if os.path.isfile( 'Z:\\TitleImage.xbx' ):	os.remove( 'Z:\\TitleImage.xbx' )
+
 	os.rename( os.path.join( Game_Path,"default.xbe" ),os.path.join( Game_Path,"game_default.xbe" ) )
 	os.rename( os.path.join( Game_Path,"loader_default.xbe" ),os.path.join( Game_Path,"default.xbe" ) )
 
@@ -225,9 +213,9 @@ if Game_Path != "":
 			if Loader == Loader3: print( 100,"Processing Game",Game_Title,"Loader Type = PD1" )
 			prepare_loaderxbe( os.path.join( Game_Path,"loader_default.xbe") )
 			time.sleep( 1 )
-			print( "480p Game Loader","","Everything is setup.","You just launch the game like normal." )
+			print( "Loader created" )
 		except:
-			print( "480p Game Loader","","You have selected a game that probably doesn't need patching","would you like to proceed?" )
+			print( "Patch not required" )
 	else:
 		print( "ERROR:","",'Please select a directory with a default.xbe present.' )
 
